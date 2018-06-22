278a54fa610562b682d994fd9f9a0b2b53e384e5
DJ Elliott <djelliottt@gmail.com>
2018-06-21 21:44:37 -0600

    update

---
+++ JSON-start/js/scripts.js
@@ -0,0 +1,17 @@
+var weatherObject = new XMLHttpRequest();
+weatherObject.open('GET', '//api.openweathermap.org/data/2.5/weather?zip=02111,us&appid=22ee2c6f5e22b73da67f53810550fb5d&units=imperial', true);
+
+weatherObject.send();
+
+weatherObject.onload = function () {
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+    
+    document.getElementById('place').innerHTML = weatherInfo.name;
+    document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('currentWind').innerHTML = weatherInfo.wind.speed;
+    
+    var iconcode = weatherInfo.weather[0].icon;
+    var icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+    document.getElementById('weather_icon').src = icon_path;
+}
