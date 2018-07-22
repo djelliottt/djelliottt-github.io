91b09018750f0f5bce9b0faff265eb3fb1b65dc3
DJ Elliott <djelliottt@gmail.com>
2018-07-22 00:04:07 -0600

    update

---
+++ MountainSpoke/scripts/serviceprice.js
@@ -0,0 +1,11 @@
+var section = document.querySelector('section');
+
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+        var threeTowns = request.response;
+        showTowns(threeTowns);
